fc099a095d394ff8e12434c6f489639e3f01b777
Jesus Araujo <neo.jando@gmail.com>
2023-05-08 22:55:36 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+const width = 8;
+const height = 30;
+const area = width * height;
+
+console.log('Value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 100;
+const payment = 250;
+const change = bill - payment;
+
+console.log('Value of change:', change);
+console.log('typof change:', typeof change);
+
+const quizzes = 85;
+const midterm = 96;
+const final = 65;
+const grade = (quizzes + midterm + final) / 3;
+
+console.log('Value of grade:', grade);
+console.log('typeof grade', typeof grade);
+
+const firstName = 'Jesus';
+const lastName = 'Araujo';
+const fullName = firstName + ' ' + lastName;
+
+console.log('Value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 88;
+const isAcidic = pH < 7;
+
+console.log('Value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 125;
+const isSparta = headCount === 300;
+
+console.log('Value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let motto = fullName;
+motto = fullName + ' is the GOAT';
+
+console.log('Value of motto:', motto);
+console.log('type of motto:', typeof motto);
