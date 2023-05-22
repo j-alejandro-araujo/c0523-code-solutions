2de862deaab002171736f186f49ded8d3c074c19
Jesus Araujo <neo.jando@gmail.com>
2023-05-22 20:56:41 +0000

    Completed exercise dom-events.

---
+++ dom-events/buttons.js
@@ -0,0 +1,31 @@
+// Click Event
+function handleClick(event) {
+  console.log('button clicked');
+  console.log('Event:', event);
+  console.log('Event Target:', event.target);
+}
+
+const singleClickButton = document.querySelector('.click-button');
+
+singleClickButton.addEventListener('click', handleClick);
+
+// Mouseover Event
+function hanldeMouseover(event) {
+  console.log('button hovered');
+  console.log('Event:', event);
+  console.log('Event Target:', event.target);
+}
+
+const mouseoverButton = document.querySelector('.hover-button');
+
+mouseoverButton.addEventListener('mouseover', hanldeMouseover);
+
+// Double-Click Event
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log('Event:', event);
+  console.log('Event Target:', event.target);
+}
+
+const dblClickButton = document.querySelector('.double-click-button');
+dblClickButton.addEventListener('dblclick', handleDoubleClick);
