78cd622d3cdb236ba1bffedd9f569588844d866b
Jesus Araujo <neo.jando@gmail.com>
2023-05-24 03:27:23 +0000

    Adjusted js file.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,19 +1,13 @@
-function instanceChange() {
-  const sphereColumn = document.querySelector('.sphere-column');
+const change = document.querySelector('.sphere-column');
 
-  sphereColumn.addEventListener('click', function onClick(event) {
-    sphereColumn.classList.toggle('instance-change');
-  });
+const sphereColumn = document.querySelector('.sphere-column');
+const background = document.querySelector('.page-container');
+
+function toggleChange() {
+  sphereColumn.classList.toggle('instance-change');
+  background.classList.toggle('background-change');
 }
 
-instanceChange();
-
-function backgroundChange() {
-  const background = document.querySelector('.page-container');
-
-  background.addEventListener('click', function onClick(event) {
-    background.classList.toggle('background-change');
-  });
-}
-
-backgroundChange();
+change.addEventListener('click', function onClick(event) {
+  toggleChange();
+});
