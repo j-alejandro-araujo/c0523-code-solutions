9560aa890d3a9655d87bdebade118fd5065ecfda
Jesus Araujo <neo.jando@gmail.com>
2023-06-06 20:40:54 +0000

    Completed javascript-new.

---
+++ javascript-new/new-objects.js
@@ -0,0 +1,23 @@
+const anObject = new Object();
+console.log('Value of anObject:', anObject);
+console.log('Typeof anObject:', typeof anObject);
+
+const anArray = new Array();
+console.log('Value of anArray:', anArray);
+console.log('Typeof anArray:', typeof anArray);
+
+const aFunction = new Function();
+console.log('Value of aFunction:', aFunction);
+console.log('Typeof aFunction:', typeof aFunction);
+
+const aString = new String();
+console.log('Value of aString:', aString);
+console.log('Typeof aString:', typeof aString);
+
+const aNumber = new Number();
+console.log('Value of aNumber:', aNumber);
+console.log('Typeof aNumber:', typeof aNumber);
+
+const aBoolean = new Boolean();
+console.log('Value of aBoolean:', aBoolean);
+console.log('Typof aBoolean', typeof aBoolean);
