636167d30d073cf364394cea73ed880c696248a7
Jesus Araujo <neo.jando@gmail.com>
2023-06-26 17:51:12 +0000

    Made flagged adjustments to js file.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -8,11 +8,11 @@ function uniqueLinear(words) {
     i < words.length; // _ * _ = O(?) --> 2 * n = O(n)
     i++ // _ * _ = O(?) --> 2 * n = O(n)
   ) {
-    const word = words[i]; // _ * _ = O(?) --> 2 * 1 = O(1)
+    const word = words[i]; // _ * _ = O(?) --> 2 * n = O(n)
     if (!seen[word]) {
-      // _ * _ = O(?) --> 2 * 1 = O(1)
+      // _ * _ = O(?) --> 2 * n = O(n)
       seen[word] = true; // _ * _ = O(?) --> 2 * 1 = O(1)
-      unique[unique.length] = word; // _ * _ = O(?) --> 3 * 1 = O(1)
+      unique[unique.length] = word; // _ * _ = O(?) --> 3 * n = O(n)
     }
   }
   return unique; // _ * _ = O(?) --> 1 * 1 = O(1)
@@ -25,21 +25,21 @@ function uniqueQuadratic(words) {
     i < words.length; // _ * _ = O(?) --> 2 * n = O(n)
     i++ // _ * _ = O(?) --> 2 * n = O(n)
   ) {
-    const word = words[i]; // _ * _ = O(?) --> 2 * 1 = O(1)
-    let isUnique = true; // _ * _ = O(?) --> 1 * 1 = O(1)
+    const word = words[i]; // _ * _ = O(?) --> 2 * n = O(n)
+    let isUnique = true; // _ * _ = O(?) --> 1 * n = O(n)
     for (
       let c = 0; // _ * _ = O(?) --> 1 * n = O(n)
       c < i; // _ * _ = O(?) --> 1 * n * n = O(n^2)
       c++ // _ * _ = O(?) --> 2 * n * n = O(n^2)
     ) {
-      const comparing = words[c]; // _ * _ = O(?) --> 2 * 1 = O(1)
+      const comparing = words[c]; // _ * _ = O(?) --> 2 * n^2 = O(n^2)
       if (comparing === word) {
-        // _ * _ = O(?) --> 1 * 1 = O(1)
-        isUnique = false; // _ * _ = O(?) --> 1 * 1 = O(1)
+        // _ * _ = O(?) --> 1 * n^2 = O(n^2)
+        isUnique = false; // _ * _ = O(?) --> 1 * n^2 = O(n^2)
       }
     }
     if (isUnique) {
-      // _ * _ = O(?) --> 1 * 1 = O(1)
+      // _ * _ = O(?) --> 1 * n = O(n)
       unique[unique.length] = word; // _ * _ = O(?) --> 3 * 1 * n = O(n)
     }
   }
