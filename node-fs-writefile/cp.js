b81f2cc779136cfb8d1f4beabc003b09b4f298a1
Jesus Araujo <neo.jando@gmail.com>
2023-07-07 17:54:37 +0000

    Removed utf8 encoding.

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -4,7 +4,7 @@ async function copy() {
   try {
     const source = process.argv[2];
     const destination = process.argv[3];
-    const info = await readFile(source, 'utf8');
+    const info = await readFile(source);
 
     await writeFile(destination, info, 'utf8');
   } catch (error) {
