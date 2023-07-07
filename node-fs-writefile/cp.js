58771581b80f2053084f8c7a818a1f5c39ee3e78
Jesus Araujo <neo.jando@gmail.com>
2023-07-07 17:47:42 +0000

    Completed exercise node-fs-writefile.

---
+++ node-fs-writefile/cp.js
@@ -0,0 +1,15 @@
+import { writeFile, readFile } from 'node:fs/promises';
+
+async function copy() {
+  try {
+    const source = process.argv[2];
+    const destination = process.argv[3];
+    const info = await readFile(source, 'utf8');
+
+    await writeFile(destination, info, 'utf8');
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
+
+copy();
