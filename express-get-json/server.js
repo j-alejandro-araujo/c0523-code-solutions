2c5b3892b3a663e6594df031d568f9218c1a239e
Jesus Araujo <neo.jando@gmail.com>
2023-07-10 21:15:50 +0000

    Completed exercise express-get-json.

---
+++ express-get-json/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92,
+  },
+};
+
+const app = express();
+
+app.get('/api/grades', (req, res) => {
+  const gradeArray = [];
+
+  for (const gradeId in grades) {
+    gradeArray.push(grades[gradeId]);
+  }
+
+  res.json(gradeArray);
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
+});
