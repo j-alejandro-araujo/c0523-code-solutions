1fc94090b66312494bd2a0ec3878278e12f257ae
Jesus Araujo <neo.jando@gmail.com>
2023-07-17 22:06:38 +0000

    Completed fetch-in-react.

---
+++ fetch-in-react/my-app/src/User.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars -- Remove me */
+import { useEffect, useState } from 'react';
+import UserCard from './UserCard';
+
+export default function User({ userId, onCancel }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+  const [user, setUser] = useState();
+
+  /* your code here (hint: useEffect) */
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users/' + userId)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Returned ${response.status} status.`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUser(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, [userId]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    console.error('Fetch error:', error);
+    return <p>Error! {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <button onClick={() => onCancel()}>Close</button>
+      <UserCard user={user} />
+    </div>
+  );
+}
